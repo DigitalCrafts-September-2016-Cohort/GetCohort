7faf13dc906b12056851a3f8efa96e06863c6c68
Matthew Downs <mwdowns@gmail.com>
2016-10-19 10:52:12 -0400

    adds the add user functionality

--- server.py
+++ server.py
@@ -29,28 +29,36 @@ def home():
 
 @app.route('/all_students')
 def all_students():
-    query = db.query('''
+    query = db.query( '''
         select
-        	users.id,
-        	first_name,
-        	last_name,
-        	cohort.name
+            users.id,
+            first_name,
+            last_name
         from
-        	users,
-        	users_link_cohort,
-        	cohort,
-        	users_link_type,
-        	user_type
-        where
-        	users.id = users_link_type.user_id and
-        	users_link_type.user_type_id = user_type.id and
-        	users.id = users_link_cohort.user_id and
-        	users_link_cohort.cohort_id = cohort.id and
-        	cohort.name = 'September 2016' and
-        	user_type.type = 'Student'
-        ;
-    '''
-    )
+            users;
+    ''')
+    # query = db.query('''
+    #     select
+    #     	users.id,
+    #     	first_name,
+    #     	last_name,
+    #     	cohort.name
+    #     from
+    #     	users,
+    #     	users_link_cohort,
+    #     	cohort,
+    #     	users_link_type,
+    #     	user_type
+    #     where
+    #     	users.id = users_link_type.user_id and
+    #     	users_link_type.user_type_id = user_type.id and
+    #     	users.id = users_link_cohort.user_id and
+    #     	users_link_cohort.cohort_id = cohort.id and
+    #     	cohort.name = 'September 2016' and
+    #     	user_type.type = 'Student'
+    #     ;
+    # '''
+    # )
     result_list = query.namedresult()
     print result_list
     return render_template(
@@ -98,6 +106,36 @@ def delete():
         "users", {
         "id": id
         }
+    )
+    return redirect("/all_students")
+
+@app.route("/add")
+def add():
+    return render_template(
+        "add.html"
+    )
+
+@app.route("/add_entry", methods=["POST"])
+def add_entry():
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    email = request.form.get("email")
+    web_page = request.form.get("web_page")
+    github = request.form.get("github")
+    current_location = request.form.get("current_location")
+    available_for_work = request.form.get("available_for_work")
+    bio = request.form.get("bio")
+
+    db.insert (
+        "users",
+        first_name = first_name,
+        last_name = last_name,
+        email = email,
+        web_page = web_page,
+        github = github,
+        current_location = current_location,
+        available_for_work = available_for_work,
+        bio = bio
     )
     return redirect("/all_students")
 
