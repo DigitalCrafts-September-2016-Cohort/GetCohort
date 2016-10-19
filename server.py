e9791b5ee50893f3d27ff7df311565b1dcb67f4a
Juan A Cortes <juan.cortes@gmail.com>
2016-10-19 14:29:02 -0400

    Set up dotenv, wsgi and absolute path for template folder

--- server.py
+++ server.py
@@ -1,25 +1,21 @@
-# from dotenv import load_dotenv, find_dotenv
-# load_dotenv(find_dotenv())
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
 from flask import Flask, redirect, render_template, request, session
-
 import pg, os
-# tmp_dir = os.path.join(os.path.abspath(__file__)), 'templates')
-# app = Flask("Get Cohort", template_folder=tmp_dir)
-app = Flask("Get Cohort")
-# db = pg.DB(
-#     dbname=os.environ.get('PG_DBNAME'),
-#     host=os.environ.get('PG_HOST'),
-#     user=os.environ.get('PG_USERNAME'),
-#     passwd=os.environ.get('PG_PASSWORD')
-# )
 
-db = pg.DB(dbname = 'getcohort_db')
+db = pg.DB(
+    dbname=os.environ.get("PG_DBNAME"),
+    host=os.environ.get("PG_HOST"),
+    user=os.environ.get("PG_USERNAME"),
+    passwd=os.environ.get("PG_PASSWORD")
+)
 db.debug = True
 
+tmp_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask("Get Cohort", template_folder = tmp_dir)
 
-
-app.secret_key = "whatever"
+app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route('/')
 def home():
